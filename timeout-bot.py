4105c95abe828040cfa0a22c1790e1ee0852706b
Parker M <99286695+p-maziarz@users.noreply.github.com>
2024-06-06 21:46:35 -0700

    Create timeout-bot.py

---
+++ timeout-bot.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+TOKEN = 'ENTER API KEY HERE'
+PREFIX = '!' 
+
+bot = commands.Bot(command_prefix=PREFIX)
+
+# Dictionary to store user timeouts
+timeouts = {}
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def timeout(ctx, member: discord.Member, duration: int):
+    timeouts[member.id] = duration
+    await ctx.send(f'{member.display_name} has been timed out for {duration} seconds.')
+    await asyncio.sleep(duration)
+    del timeouts[member.id]
+    await ctx.send(f'{member.display_name} has been untimed out.')
+
+@bot.event
+async def on_message(message):
+    # Check if the author of the message is timed out
+    if message.author.id in timeouts:
+        await message.delete()
+        await message.author.send("You're currently timed out and cannot send messages.")
+    else:
+        await bot.process_commands(message)
+
+bot.run(TOKEN)
